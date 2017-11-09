b74a942b43576b2c338454a636fc2de57c4ba88b
Diego Andrade Canosa <diego.andrade@udc.es>
2017-11-09 10:55:44 +0100

    Nueva versión de views.py

--- myciao/reviews/views.py
+++ myciao/reviews/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from reviews.models import Product,Review
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hola mundo")
+	product_list=Product.objects.all()
+	output=''
+	for p in product_list:
+		output=output+'<a href=\"'+str(p.id)+'\">'+p.name+"</a><BR>"
+	return HttpResponse(output)
 
-def detail(request, product_id):
-	return HttpResponse("Hola mundo "+ product_id)
+def detail(request,product_id):
+	p=Product.objects.get(id=product_id)
+	output=p.name+'<BR><BR>'
+	reviews_list=p.review_set.all()
+	for r in reviews_list:
+		output=output+r.title+'<BR>'+r.text+'<BR><BR>'
+	return HttpResponse(output)
