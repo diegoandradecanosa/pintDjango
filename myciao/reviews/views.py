e86405a3ff1c5edb9114ac601ac79006bc4a279c
Diego Andrade Canosa <diego.andrade@udc.es>
2017-11-09 12:16:25 +0100

    Añadida la primera versión con plantillas

--- myciao/reviews/views.py
+++ myciao/reviews/views.py
@@ -7,15 +7,12 @@ from reviews.models import Product,Review
 
 def index(request):
 	product_list=Product.objects.all()
-	output=''
-	for p in product_list:
-		output=output+'<a href=\"'+str(p.id)+'\">'+p.name+"</a><BR>"
-	return HttpResponse(output)
+	context={'product_list':product_list}
+	return render(request,'reviews/index.html',context)
+
 
 def detail(request,product_id):
 	p=Product.objects.get(id=product_id)
-	output=p.name+'<BR><BR>'
 	reviews_list=p.review_set.all()
-	for r in reviews_list:
-		output=output+r.title+'<BR>'+r.text+'<BR><BR>'
-	return HttpResponse(output)
+	context={'p':p,'reviews_list':reviews_list}
+	return render(request,'reviews/product_detail.html',context)
