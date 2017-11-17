c1198678978c0cbd3c89dfc4b446bf62f27e29a8
Diego Andrade Canosa <diego.andrade@udc.es>
2017-11-17 12:57:18 +0100

    En proceso paso 7

--- myciao/reviews/forms.py
+++ myciao/reviews/forms.py
@@ -1,6 +1,22 @@
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Layout, ButtonHolder
+
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            'username',
+            'password',
+            ButtonHolder(
+                Submit('login', 'Login', css_class='btn-primary')
+            )
+        )
 
 
 class NewProductForm(forms.Form):
--- myciao/reviews/views.py
+++ myciao/reviews/views.py
@@ -2,20 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from reviews.models import Product,Review
-from reviews.forms import NewProductForm, NewReviewForm
+from reviews.forms import NewProductForm, NewReviewForm, LoginForm
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
-# Create your views here.
+def logout_view(request,next):
+	logout(request)
+	return redirect(next)
 
 def index(request):
+	loginError=""
+
 	if 'name' in request.POST:
 		name=request.POST['name']
 		barcode=request.POST['barcode']
 		p=Product(name=name,barcode=barcode)
 		p.save()
+	if 'username' in request.POST:
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)		
+		if user is not None:
+			request.user=user	
+		else:
+			loginError="Error de login"			
+
 	product_list=Product.objects.all()
 	newProductForm=NewProductForm()
-	context={'product_list':product_list,'new_product_form':newProductForm}
+	loginForm=LoginForm()
+	
+
+	if request.user.is_authenticated:
+		context={'product_list':product_list,'new_product_form':newProductForm,'user':request.user,'login_form':loginForm,'loginError':loginError}
+	else:
+		context={'product_list':product_list,'new_product_form':newProductForm,'login_form':loginForm,'loginError':loginError}
+	
 	
 	return render(request,'reviews/index.html',context)
 
@@ -23,11 +45,30 @@ def index(request):
 def detail(request,product_id):
 	p=Product.objects.get(id=product_id)
 	reviews_list=p.review_set.all()
+	loginError=""
+
 	if 'title' in request.POST:
 		title=request.POST['title']
 		text=request.POST['text']
 		p.review_set.create(title=title,text=text)
+	if 'username' in request.POST:
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)	
+		if user is not None:
+			request.user=user
+		else:
+			loginError="Error de login"
+
 	newReviewForm=NewReviewForm()		
 	newReviewForm.helper.form_action = reverse('detail', kwargs={'product_id': p.id})		
-	context={'p':p,'reviews_list':reviews_list,'new_review_form':newReviewForm}
+	loginForm=LoginForm()
+
+
+	if request.user.is_authenticated:
+		context={'p':p,'reviews_list':reviews_list,'new_review_form':newReviewForm,'user':request.user,'login_form':loginForm,'loginError':loginError}
+	else:
+		context={'p':p,'reviews_list':reviews_list,'new_review_form':newReviewForm,'login_form':loginForm,'loginError':loginError}
+
+
 	return render(request,'reviews/product_detail.html',context)
