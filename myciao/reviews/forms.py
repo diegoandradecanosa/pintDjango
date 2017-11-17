626edc2c382b247502ba20388dd8595df1d3e3dd
Diego Andrade Canosa <diego.andrade@udc.es>
2017-11-17 11:19:21 +0100

    Paso 6

---
+++ myciao/reviews/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+class NewProductForm(forms.Form):
+    barcode= forms.CharField(label="Código de barras",
+            max_length=32,
+            required=True
+            )
+    name= forms.CharField(label="Nombre",
+            max_length=140,
+            required=True
+            )
+
+    def __init__(self, *args, **kwargs):
+        super(NewProductForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()            
+        self.helper.form_id = 'id-newProductForm'
+        self.helper.form_class = 'form-group'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'index'
+        self.helper.add_input(Submit('submit', 'Enviar'))     
+
+class NewReviewForm(forms.Form):
+    title= forms.CharField(label="Título",
+            max_length=50,
+            required=True
+            )
+    text= forms.CharField(label="Texto",
+            max_length=500,
+            required=True
+            )
+
+    def __init__(self, *args, **kwargs):
+        super(NewReviewForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()            
+        self.helper.form_id = 'id-newReviewForm'
+        self.helper.form_class = 'form-group'
+        self.helper.form_method = 'post'
+#        self.helper.form_action = 'product_detail'
+        self.helper.add_input(Submit('submit', 'Enviar'))             
+
+
--- myciao/reviews/views.py
+++ myciao/reviews/views.py
@@ -2,17 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from reviews.models import Product,Review
+from reviews.forms import NewProductForm, NewReviewForm
+from django.urls import reverse
 
 # Create your views here.
 
 def index(request):
+	if 'name' in request.POST:
+		name=request.POST['name']
+		barcode=request.POST['barcode']
+		p=Product(name=name,barcode=barcode)
+		p.save()
 	product_list=Product.objects.all()
-	context={'product_list':product_list}
+	newProductForm=NewProductForm()
+	context={'product_list':product_list,'new_product_form':newProductForm}
+	
 	return render(request,'reviews/index.html',context)
 
 
 def detail(request,product_id):
 	p=Product.objects.get(id=product_id)
 	reviews_list=p.review_set.all()
-	context={'p':p,'reviews_list':reviews_list}
+	if 'title' in request.POST:
+		title=request.POST['title']
+		text=request.POST['text']
+		p.review_set.create(title=title,text=text)
+	newReviewForm=NewReviewForm()		
+	newReviewForm.helper.form_action = reverse('detail', kwargs={'product_id': p.id})		
+	context={'p':p,'reviews_list':reviews_list,'new_review_form':newReviewForm}
 	return render(request,'reviews/product_detail.html',context)
