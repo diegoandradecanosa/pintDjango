9c16a917492d8d9bf5d974580d1cae9a44d9f22a
diego.andrade <diego.andrade@udc.es>
2018-11-19 14:50:58 +0100

    Undoing

--- myciao/reviews/views.py
+++ myciao/reviews/views.py
@@ -6,16 +6,7 @@ from reviews.models import Product,Review
 # Create your views here.
 
 def index(request):
-	product_list=Product.objects.all()
-	output=''
-	for p in product_list:
-		output=output+'<a href=\"'+str(p.id)+'\">'+p.name+"</a><BR>"
-	return HttpResponse(output)
+	return HttpResponse("Hola mundo")
 
 def detail(request,product_id):
-	p=Product.objects.get(id=product_id)
-	output=p.name+'<BR><BR>'
-	reviews_list=p.review_set.all()
-	for r in reviews_list:
-		output=output+r.title+'<BR>'+r.text+'<BR><BR>'
-	return HttpResponse(output)
+	return HttpResponse("Hola mundo "+product_id)
