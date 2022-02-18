1414abdc15dc072831c664ce637f0a1198b9accb
Diego Andrade <diego.andrade@udc.es>
2022-02-18 10:02:48 +0000

    Fixing some problems with django4

--- myciao/reviews/urls.py
+++ myciao/reviews/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns=[
 	path('', views.index, name='index'),
-	path('<str:product_id>/',views.detail,name='detail'),
+	path('<int:product_id>/',views.detail,name='detail'),
 	path('logout/', views.logout_view, name='logout_view'),
 	path('signup/', views.signup_view, name='signup_view'),
 ]
